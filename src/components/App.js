234e848cbf2510682b1497d6255a791d6198a6b9
renarsaar <renar.saaremets@gmail.com>
2020-07-07 13:06:37 +0300

    redirect 404 page

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, BrowserRouter } from "react-router-dom";
+import { Route, BrowserRouter, Redirect } from "react-router-dom";
 
 import VideoPlayer from "./VideoPlayer";
 import MusicPlayer from "./MusicPlayer";
@@ -9,6 +9,7 @@ const App = () => {
     <BrowserRouter>
       <Route path="/player/" exact component={VideoPlayer} />
       <Route path="/player/music" exact component={MusicPlayer} />
+      <Redirect to="/player/" />
     </BrowserRouter>
   );
 };
